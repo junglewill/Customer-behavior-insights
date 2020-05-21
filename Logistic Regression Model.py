be7a174247d5aa89cd5e21f662910492d8f17889
William Lee <51158836+junglewill@users.noreply.github.com>
2020-05-21 23:59:07 +0800

    Update Logistic Regression Model.py

--- Logistic Regression Model.py
+++ Logistic Regression Model.py
@@ -14,16 +14,16 @@ X = one_hot_data.iloc[:, :-1]
 # y = target values, last column of the data frame
 y = one_hot_data.iloc[:, -1]
 
-# 建立模型
+# build up the model
 logistic_regr = linear_model.LogisticRegression()
 logistic_regr.fit(X, y)
 
-# 印出 p-value
+# print out p-value and the coefficient to check the relationship of each variable
 print(f_regression(X, y)[1])
 
 print(logistic_regr.coef_)
 
-# 計算準確率
+# calculate the precision rate
 NtoA_prediction = logistic_regr.predict(X)
 accuracy = logistic_regr.score(X, y)
 print(accuracy)
@@ -39,16 +39,16 @@ new_X = new_one_hot_data.iloc[:, :-1]
 # y = target values, last column of the data frame
 new_y = new_one_hot_data.iloc[:, -1]
 
-# 建立模型
+# build up the model
 new_logistic_regr = linear_model.LogisticRegression()
 new_logistic_regr.fit(new_X, new_y)
 
-# 印出 p-value
+# print out p-value
 print(f_regression(new_X, new_y)[1])
 
 print(new_logistic_regr.coef_)
 
-# 計算準確率
+# calculate the precision rate
 NtoA_prediction = new_logistic_regr.predict(new_X)
 new_accuracy = new_logistic_regr.score(new_X, new_y)
 print(new_accuracy)
